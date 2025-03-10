6da9b67c74fe5dc313cdaefa7731a548c5dd3395
NanaAisha217 <71590329+NanaAisha217@users.noreply.github.com>
2025-03-10 12:44:57 +0100

    Add files via upload

---
+++ StackProject/StackProject/Models/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProject.Models
+{
+    class Program
+    {
+        static void Main()
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop()); // 3
+            Console.WriteLine(stack.Pop()); // 2
+            Console.WriteLine(stack.Pop()); // 1
+        }
+    }
+}
---
+++ StackProject/StackProject/Models/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProject.Models
+{
+   public class Stack
+    {
+        private List<object> _items = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("Cannot add null to the stack.");
+            _items.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (_items.Count == 0)
+                throw new InvalidOperationException("Stack is empty.");
+
+            object lastItem = _items[_items.Count - 1];
+            _items.RemoveAt(_items.Count - 1);
+            return lastItem;
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+    }
+}
